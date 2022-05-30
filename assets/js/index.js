9a1fdee6ae90b935e0eb36a2a3c7d1ba26e6269b
MyCandy160 <3087027139@qq.com>
2022-05-30 20:31:51 +0800

    index

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+function getUserInfo() {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    success: (res) => {
+      // console.log(res);
+      const { data, status, message } = res;
+      if (status !== 0) return layer.msg(message);
+      layer.msg(message);
+      renderAvatar(data);
+    },
+  });
+}
+const renderAvatar = (uers) => {
+  console.log(uers);
+  let uname = uers.nickname || uers.username;
+  $("#welcome").html(`欢迎${uname}`);
+  if (uers.user_pic !== null) {
+    $(".layui-nav-img").attr("src", uers.user_pic);
+    $(".text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    $(".text-avatar").html(uname[0].toUpperCase());
+  }
+};
+$("#deleteBtnOut").click(() => {
+  layer.confirm("真的要退出吗？", { icon: 3, title: "提示" }, (index) => {
+    localStorage.removeItem("token");
+    location.href = "/login.html";
+    // layer.close(index);
+  });
+});
+getUserInfo();
